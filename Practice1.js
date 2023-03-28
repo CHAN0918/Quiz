a5da1a861bd62770be6c1e7db6e070ae298f50bc
CHAN0918 <B022110113@student.utem.edu.my>
2023-03-28 08:09:38 +0800

    ...

--- Practice1.js
+++ Practice1.js
@@ -1,16 +1,16 @@
 let data1 = 1
-let data2 = "1"
+let data2 = "1.23"
 console.log(data1, typeof data1)
 console.log(data2, typeof data2)
 
 let data3 = data1.toString + parseInt(data2)
 console.log(data3, typeof data3)
 
-//array
+//array data
 let data =["One",2, true]
 console.log(data[2], typeof data[2])
 
-//object 
+//object data
 let user ={name:"Soo",faculty:"FKEKK",phone:"12345",email:"soo@utem.edu.my"}
 console.log (user.faculty)
 
